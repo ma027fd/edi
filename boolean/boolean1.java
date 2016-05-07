8183ed1005c2fe7414c020ef8657e5dee4415156
Marco Kotzian <kotzian@pc192-168-2-101.speedport_w_724v_typ_a_05011603_00_009>
2016-05-07 12:18:33 +0200

    First commit

---
+++ boolean/boolean1.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class boolean1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import inout.Console;
+
+public class boolean1
+{
+    public static void main (String args[]){
+        System.out.println("Wert für lichtschalter_1 eingeben:");
+        boolean lichtschalter_1 = Console.readBoolean();
+        System.out.println("Wert für lichtschalter_2 eingeben:");
+        boolean lichtschalter_2 = Console.readBoolean();
+        boolean licht_an = lichtschalter_1 ^ lichtschalter_2;
+        System.out.println("licht_an hat den wert: "+licht_an);
+        return;
+    }
+}
